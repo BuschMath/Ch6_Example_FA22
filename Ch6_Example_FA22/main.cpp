73bc8407a5a68b120d7335c9e0e8ab1cd1440486
BuschMath <busch@iowacentral.edu>
2022-10-07 10:11:21 -0500

    Add project files.

---
+++ Ch6_Example_FA22/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Counting program:\n";
+	int count = 1;
+
+	while (count <= 100)
+	{
+		cout << count << endl;
+		count = count + 1;
+	}
+
+	return 0;
+}
